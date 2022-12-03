ce63eafedb69cb9e81fbde7f83abb85ba3096a3d
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-12-03 16:33:33 -0600

    Fin de la decodificacion

--- HuffmanDec.m
+++ HuffmanDec.m
@@ -14,3 +14,16 @@ HDecode=uint8(HDecode);
 %Convertimos el vector a un array
 dec_f=sqrt(length(HDecode));
 dec_c=dec_f;
+
+%Creamos la matriz
+tam_vec=1;
+
+for x=1:f
+    for y=1:c
+        img(x,y)=HDecode(tam_vec);
+        tam_vec=tam_vec+1;
+    end
+end
+
+figure
+imshow(img)
